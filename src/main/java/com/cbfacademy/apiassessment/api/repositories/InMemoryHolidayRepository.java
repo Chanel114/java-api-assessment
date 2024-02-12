5ba481511cedbdbe733b108290fa6db633b9666d
Chanel114 <25chanelrobinson@gmail.com>
2024-02-12 20:32:31 +0000

    create implementation of Holiday Repository class

---
+++ src/main/java/com/cbfacademy/apiassessment/api/repositories/InMemoryHolidayRepository.java
@@ -0,0 +1,6 @@
+package com.cbfacademy.apiassessment.api.repositories;
+
+//Implementation of Holiday Repository methods
+public class InMemoryHolidayRepository implements HolidayRepository {
+    
+}
