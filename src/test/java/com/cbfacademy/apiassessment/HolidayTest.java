0fbdc5c93f331b4c1b657ac5b8276773acd4a842
Chanel114 <25chanelrobinson@gmail.com>
2024-02-13 22:21:32 +0000

    add comments explaining test

--- src/test/java/com/cbfacademy/apiassessment/HolidayTest.java
+++ src/test/java/com/cbfacademy/apiassessment/HolidayTest.java
@@ -9,21 +9,28 @@ import org.junit.jupiter.api.Test;
 import com.cbfacademy.apiassessment.api.entities.Holiday;
 
 public class HolidayTest {
+
+     
     
     @Test
     void daysUntilNextHoliday() {
-        //Create an instance of the Holiday class
+//Create an instance of the Holiday class
         Holiday holiday = new Holiday();
 
 
 //set a future date for the holiday (5 days from today) (simulates a scenario where holiday is in the future)
     holiday.setDate(LocalDate.now().plusDays(5)); 
     
-    //Call the daysUntilNextHoliday method to calculate the days until the next holiday date (under test)
+//WHEN
+//Call the daysUntilNextHoliday method to calculate the days until the next holiday date (under test)
         int daysUntilNextHoliday = holiday.daysUntilNextHoliday();
-    //Asserts that the calculated days are equal to the expected value (5)
+//THEN
+//Asserts that the calculated days are equal to the expected value (5)
         assertEquals(5, daysUntilNextHoliday); 
 
         
     }
+
+
+
 }
