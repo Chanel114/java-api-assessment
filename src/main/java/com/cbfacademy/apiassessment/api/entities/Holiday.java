06c65527ee0da9c93f1521d8919ae6bb9adb54b4
Chanel114 <25chanelrobinson@gmail.com>
2024-02-13 19:10:18 +0000

    Add basic Holiday class with variables and add LocalDate import

--- src/main/java/com/cbfacademy/apiassessment/api/entities/Holiday.java
+++ src/main/java/com/cbfacademy/apiassessment/api/entities/Holiday.java
@@ -1,5 +1,12 @@
 package com.cbfacademy.apiassessment.api.entities;
 
+import java.time.LocalDate;
+
 public class Holiday {
-    
+    private String name;
+    private LocalDate date;
+
+    //Constructors, getters and setters
+
+    // Algorithm that calculates days until next holiday
 }
