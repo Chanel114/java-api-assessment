4e2095817c450dafa3b12e680cb3041bc0f07e63
Chanel114 <25chanelrobinson@gmail.com>
2024-02-13 22:31:51 +0000

    add conrtuctor method with algorithm

--- src/main/java/com/cbfacademy/apiassessment/api/entities/Holiday.java
+++ src/main/java/com/cbfacademy/apiassessment/api/entities/Holiday.java
@@ -1,5 +1,23 @@
 package com.cbfacademy.apiassessment.api.entities;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class Holiday {
+    private LocalDate date;
+
+    // Constructors, getters and setters
+    //Algorithm to calculate days until the next holiday
+    public int daysUntilNextHoliday(){
+    LocalDate currentDate = LocalDate.now();
+        return (int)
+    ChronoUnit.DAYS.between (currentDate, date);
+    }
+    
+
+
+    public void setDate(LocalDate plusDays) {
+        throw new UnsupportedOperationException("Unimplemented method 'setDate'");
+    }
     
 }
