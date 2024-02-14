6f12443757f5ee6e85cfc51f46fa6303bc8c492e
Chanel114 <25chanelrobinson@gmail.com>
2024-02-14 18:29:08 +0000

    successfully Modify Holiday repo and add add AnotherHolidayAndCheckIfPresent test

--- src/main/java/com/cbfacademy/apiassessment/api/repositories/InMemoryHolidayRepository.java
+++
@@ -1,6 +0,0 @@
-package com.cbfacademy.apiassessment.api.repositories;
-
-//Implementation of Holiday Repository methods
-public class InMemoryHolidayRepository implements HolidayRepository {
-    
-}
--- src/test/java/com/cbfacademy/apiassessment/HolidayTest.java
+++ src/test/java/com/cbfacademy/apiassessment/HolidayTest.java
@@ -1,18 +1,23 @@
 package com.cbfacademy.apiassessment;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 
+
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import com.cbfacademy.apiassessment.api.entities.Holiday;
+import com.cbfacademy.apiassessment.api.repositories.InMemoryHolidayRepositoryimpl;
 
 public class HolidayTest {
     //Create an instance of the Holiday class
     Holiday holiday = new Holiday();
 
     @Test
+    @Order(1)
     void daysUntilNextHoliday() {
 //GIVEN
 //set a future date for the holiday (5 days from today) (simulates a scenario where holiday is in the future)
@@ -26,5 +31,19 @@ public class HolidayTest {
         assertEquals(5, daysUntilNextHoliday); 
 
     }
+    @Test
+    @Order(2)
+    void addAnotherHolidayAndCheckIfPresent() {
+        InMemoryHolidayRepositoryimpl holidayRepository = new InMemoryHolidayRepositoryimpl();
+
+        Holiday holiday = new Holiday();
+        holiday.setDate(LocalDate.now().plusDays(10));
+        holidayRepository.addHoliday(holiday);
+
+        assertTrue(holidayRepository.getAllHolidays().contains(holiday));
+    }
+
+
+
 
 }
