ce03e73a9a194838d2f0d2307f29992eb0972ef7
Chanel114 <25chanelrobinson@gmail.com>
2024-02-15 00:23:32 +0000

    create more holiday constructors

--- src/main/java/com/cbfacademy/apiassessment/api/entities/Holiday.java
+++ src/main/java/com/cbfacademy/apiassessment/api/entities/Holiday.java
@@ -4,43 +4,76 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class Holiday {
+    //instance variables only be access in this class (getters and setters get/set the values)
     private String name; //reps holiday name E.g christmas
     private LocalDate date; // reps date of holiday
     private boolean publicHoliday; // boolean flag indicates if is public holiday
 
-//Constructor
-    public Holiday(String name, LocalDate date, boolean publicHoliday){
-        this.name = name;
-        this.date = date;
+//Constructors (general object maker to make specific object maker (versioned object))
+
+public Holiday (){
+
+}
+   //use this constructor for test
+public Holiday(boolean publicHoliday){
+
+}
+    
+
+public Holiday(String name, LocalDate date, boolean publicHoliday){
+        this.setName(name); // controls under rules of setName setter method - specific holiday names only
+        this.setDate(date); // controls under rules of setDate setter method
         this.publicHoliday = publicHoliday;
+}
+
+
+
+//Getter then setter method for public holiday
+public boolean isPublicHoliday(){
+          return publicHoliday;
     }
 
-    //Getter then setter method for public holiday
-    public boolean isPublicHoliday(){
-        return publicHoliday;
-    }
-
-    public void setPublicHoliday(boolean publicHoliday){
-        this.publicHoliday = publicHoliday;
+public void setPublicHoliday(boolean publicHoliday){
+          this.publicHoliday = publicHoliday;
         
     }
 
-    //Setter method for holiday date
-    public void setDate(LocalDate date) {
+//Setter method for holiday date
+public void setDate(LocalDate date) {
         this.date = date;
     }
-
-    //Algorithm to calculate days until the next holiday
-        public int daysUntilNextHoliday(){
-        LocalDate currentDate = LocalDate.now();
-            return (int)
-        ChronoUnit.DAYS.between (currentDate, date);
+    
+//Getter method for local date (current dat)
+public LocalDate getDate(){
+       return LocalDate.now().plusDays(5);
     }
+    
+//Algorithm to calculate days until the next holiday
+public int daysUntilNextHoliday(){
+    LocalDate currentDate = LocalDate.now();
+    return (int)
+    //calculates the number of days between the current date and the specified date (date). 
+    ChronoUnit.DAYS.between (currentDate, date);
+    }
+    //reps difference in days
 
     //Getter Method for Holiday name
     public String getName(){
     return name;
+    }
+
+    //Setter method for holiday date
+    public void setName(String name) {
+        this.name = name;
+
+
 }
 
+
+
+@Override
+public String toString(){
+    return "Holiday" + " " + name + " " + date + " " + publicHoliday;  
+}
      }
 
