fb712b7a5e678cd0a81f80e30e3335e13e5d0473
Chanel114 <25chanelrobinson@gmail.com>
2024-02-15 01:53:13 +0000

    reverted entry point getmapping to greetings (Hello World)

--- src/main/java/com/cbfacademy/apiassessment/main/App.java
+++ src/main/java/com/cbfacademy/apiassessment/main/App.java
@@ -1,16 +1,10 @@
 package com.cbfacademy.apiassessment.main;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.cbfacademy.apiassessment.api.entities.Holiday;
 
 @SpringBootApplication
 @RestController
@@ -19,24 +13,9 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 
-		Holiday h1 = new Holiday("Valentine's Day", LocalDate.now().plusDays(5), true);
-		Holiday h2 = new Holiday("St.Patricks Day", LocalDate.now().plusDays(5), false);
-		System.out.println(h1);
-		System.out.println(h2);
 	}
-
-	@GetMapping("/holidays")
-	@ResponseBody
-	public List<Holiday> hello() {
-		return List.of(
-			new Holiday("Valentine's Day",
-							 LocalDate.now().plusDays(5),
-							 false));
+	@GetMapping("/greetings")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	return String.format("Hello %s", name);
 	}
-	
-	
-	
-	//public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-	//	return String.format("Hello %s", name);
-	}
-
+}
