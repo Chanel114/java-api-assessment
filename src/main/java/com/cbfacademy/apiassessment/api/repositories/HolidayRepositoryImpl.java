c4ee28a177161954c7ee0ba78dafeb8154e59e63
Chanel114 <25chanelrobinson@gmail.com>
2024-02-17 18:14:50 +0000

    Integrate JSON file as initial data source

--- src/main/java/com/cbfacademy/apiassessment/api/repositories/HolidayRepositoryImpl.java
+++ src/main/java/com/cbfacademy/apiassessment/api/repositories/HolidayRepositoryImpl.java
@@ -1,13 +1,36 @@
 package com.cbfacademy.apiassessment.api.repositories;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.cbfacademy.apiassessment.api.entities.Holiday;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class HolidayRepositoryImpl implements HolidayRepository{
 //List to store holiday objects in memory initialised by empty array list
 private List<Holiday> holidays = new ArrayList<>();
+
+ // Constructor to load holiday data from JSON file during initialization
+//  The `loadHolidayData` method reads the data from the "holidays.json" file in the classpath and initializes the in-memory holiday list
+ HolidayRepositoryImpl() {
+        loadHolidayData();
+    }
+
+    // Method to load holiday data from the JSON file
+    private void loadHolidayData() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<Holiday>> typeReference = new TypeReference<>() {};
+        // Read JSON data into a list of Holiday objects
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("holidays.json")) {
+            holidays = objectMapper.readValue(inputStream, typeReference);
+
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception appropriately in a real application
+        }
+    }
 
     //implement get all holiday method from holiday repo interface 
     @Override
