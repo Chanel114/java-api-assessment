f1ea84cddecc22b3e1c47e694a55e463a968866e
Chanel114 <25chanelrobinson@gmail.com>
2024-02-17 22:07:52 +0000

    add endpoints for get all holiday list. Get a holiday by name and days until the nect holiday from local date

--- src/main/java/com/cbfacademy/apiassessment/api/controllers/HolidayController.java
+++ src/main/java/com/cbfacademy/apiassessment/api/controllers/HolidayController.java
@@ -2,24 +2,60 @@ package com.cbfacademy.apiassessment.api.controllers;
 
 
 
- import java.time.LocalDate;
- import java.util.List;
+//  import java.time.LocalDate;
+import java.util.List;
 
- import org.springframework.web.bind.annotation.GetMapping;
- import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
  import com.cbfacademy.apiassessment.api.entities.Holiday;
+import com.cbfacademy.apiassessment.api.service.HolidayService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 
 @RestController
-public class HolidayController {
-    @GetMapping("api/v1/holidays")
-     public List<Holiday> holiday() {
-         return List.of(
-           new Holiday("Valentine's Day",
-                           LocalDate.now().plusDays(5),
-                             false));
+@RequestMapping("api/v1/holidays")
 
- }
+public class HolidayController {
+
+private final HolidayService holidayService;
+
+public HolidayController(HolidayService holidayService){
+  this.holidayService = holidayService;
 }
+
+  @GetMapping
+  public List<Holiday> getAllHolidays(){
+    return holidayService.getAllHolidays();
+  }
+
+  
+  @GetMapping("/{name}")
+  public Holiday getHolidayByName(@PathVariable String name) {
+    return holidayService.getHolidayByName(name);
+  }
+
+  @GetMapping("/{name}/daysUntilNext")
+  public int getDaysUntilNextHoliday(@RequestParam String name) {
+      return holidayService.daysUntilNextHoliday(name);
+  }
+  
+// for test 
+
+//     @GetMapping("api/v1/holidays")
+//      public List<Holiday> holiday() {
+//          return List.of(
+//            new Holiday("Valentine's Day",
+//                            LocalDate.now().plusDays(5),
+//                              false));
+
+//  }
+
+}
+
