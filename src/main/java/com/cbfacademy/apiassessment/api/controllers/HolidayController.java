5cbca2205f15a0c8946f07320f66f7b6b1f1fa48
Chanel114 <25chanelrobinson@gmail.com>
2024-02-18 02:58:02 +0000

    change to ResponseEntity class for more streamline http response

--- src/main/java/com/cbfacademy/apiassessment/api/controllers/HolidayController.java
+++ src/main/java/com/cbfacademy/apiassessment/api/controllers/HolidayController.java
@@ -1,55 +1,64 @@
 package com.cbfacademy.apiassessment.api.controllers;
 
 
-
-import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
- import com.cbfacademy.apiassessment.api.entities.Holiday;
+import com.cbfacademy.apiassessment.api.entities.Holiday;
+import com.cbfacademy.apiassessment.api.exceptions.HolidayNotFoundException;
 import com.cbfacademy.apiassessment.api.service.HolidayService;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
-
-
 @RestController
-@RequestMapping("api/holidays")
-
+@RequestMapping("holidays")
 public class HolidayController {
 
-private final HolidayService holidayService;
+@Autowired
+private HolidayService holidayService;
 
 public HolidayController(HolidayService holidayService){
   this.holidayService = holidayService;
 }
 
-  @GetMapping("/names")
-public List<String> getAllHolidayNames() {
+  @GetMapping
+public ResponseEntity<List<String>> getAllHolidayNames() {
     List<Holiday> holidays = holidayService.getAllHolidays();
     List<String> holidayNames = holidays.stream()
                                        .map(Holiday::getName)
                                        .collect(Collectors.toList());
-    return holidayNames;
+    return new ResponseEntity<>(holidayNames, HttpStatus.OK);
 }
 
 
-  
   @GetMapping("/{name}")
-  public Holiday getHolidayByName(@PathVariable String name) {
-    return holidayService.getHolidayByName(name);
+  public ResponseEntity<Holiday> getHolidayByName(@PathVariable String name) {
+    Holiday holiday = holidayService.getHolidayByName(name);
+    if (holiday != null) {
+      return new ResponseEntity<>(holiday, HttpStatus.OK);
+    } else {
+    return new ResponseEntity<>(holiday, HttpStatus.NOT_FOUND);
+    }
   }
 
   @GetMapping("/{name}/daysUntilNext")
-  public LocalDate getDaysUntilNextHoliday(@PathVariable String name) {
-      return holidayService.daysUntilNextHoliday(name);
+  public ResponseEntity<Long> getDaysUntilNextHoliday(@PathVariable String name) {
+    try {
+      Long daysUntilNextHoliday = holidayService.daysUntilNextHoliday(name);
+      return new ResponseEntity<>(daysUntilNextHoliday, HttpStatus.OK);
+    } catch (HolidayNotFoundException e) {
+      //handle if the holiday is not found
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
   }
-  
 }
 
