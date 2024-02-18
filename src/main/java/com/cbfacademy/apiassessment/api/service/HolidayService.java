9cf87ca28e4cc788baff665a4e9a070e689e90cd
Chanel114 <25chanelrobinson@gmail.com>
2024-02-18 02:58:49 +0000

    code cleanup

--- src/main/java/com/cbfacademy/apiassessment/api/service/HolidayService.java
+++ src/main/java/com/cbfacademy/apiassessment/api/service/HolidayService.java
@@ -1,6 +1,6 @@
 package com.cbfacademy.apiassessment.api.service;
 
-import java.time.LocalDate;
+
 import java.util.List;
 
 import com.cbfacademy.apiassessment.api.entities.Holiday;
@@ -8,5 +8,5 @@ import com.cbfacademy.apiassessment.api.entities.Holiday;
 public interface HolidayService {
     List<Holiday> getAllHolidays();
     Holiday getHolidayByName(String name);
-    LocalDate daysUntilNextHoliday(String name);
+    Long daysUntilNextHoliday(String name);
 }
--- src/test/java/com/cbfacademy/apiassessment/HolidayTest.java
+++ src/test/java/com/cbfacademy/apiassessment/HolidayTest.java
@@ -1,8 +1,6 @@
 package com.cbfacademy.apiassessment;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
